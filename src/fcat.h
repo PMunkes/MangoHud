71f2f2ab66a5c20c700f850e88382773d978c8ce
PMunkes <PMunkes@users.noreply.github.com>
2022-04-19 19:37:29 +0200

    Create fcat.h

---
+++ src/fcat.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef MANGOHUD_FCAT_H
+#define MANGOHUD_FCAT_H
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include <condition_variable>
+#include <array>
+
+#include "timing.hpp"
+
+#include "overlay_params.h"
+
+struct fcatoverlay{
+  bool is_active=true;
+  unsigned short sreen_edge=3;
+  size_t currentColor=0;
+  static const std::array<const ImColor,16> sequence={{{255, 255, 255},{0, 255, 0},{0, 0, 255},{255, 0, 0},{0, 128, 128},{0, 0, 128},{0, 128, 0},{0, 255, 255},{128, 0, 0},{192, 192, 192},{128, 0, 128},{128, 128, 0},{128, 128, 128},{255, 0, 255},{255, 255, 0},{255, 128, 0}}};
+};
+
+#endif
